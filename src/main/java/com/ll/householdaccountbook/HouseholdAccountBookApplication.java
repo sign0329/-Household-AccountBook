2856d747f4b094e4627e49d08abf60c7ef66e01d
sign0329 <sign0329@gmail.com>
2025-12-31 15:46:42 +0900

    프로젝트 생성

---
+++ src/main/java/com/ll/householdaccountbook/HouseholdAccountBookApplication.java
@@ -0,0 +1,13 @@
+package com.ll.householdaccountbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseholdAccountBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HouseholdAccountBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/householdaccountbook/HouseholdAccountBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.householdaccountbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseholdAccountBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
